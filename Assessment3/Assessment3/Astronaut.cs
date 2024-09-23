8f2d4b6c9738aa32853c1cafd47d7c1f5094ae7f
Joshua DiChello <jddichello@gmail.com>
2024-09-23 18:23:39 -0500

    completed assessment 3

---
+++ Assessment3/Assessment3/Astronaut.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment3
+{
+    public class Astronaut
+    {
+        public string SuitColor { get; set; }
+        public bool IsTraitor { get; set; }
+        public Astronaut(string color, bool isTraitor)
+        {
+            SuitColor = color;
+            IsTraitor = isTraitor;
+        }
+    }
+}
---
+++ Assessment3/Assessment3/SpaceShip.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assessment3
+{
+    public class SpaceShip
+    {
+        public List<Astronaut> Astronauts { get; private set; }
+        public SpaceShip()
+        {
+            Astronauts = new List<Astronaut>();
+            {
+                new Astronaut("Red", false);
+                new Astronaut("Blue", false);
+                new Astronaut("Green", false);
+                new Astronaut("Pink", false);
+                new Astronaut("Orange", false);
+                new Astronaut("Yellow", false);
+                new Astronaut("Black", false);
+                new Astronaut("White", false);
+                new Astronaut("Purple", true);
+                new Astronaut("Brown", true);
+            }
+        }
+        public void RemoveAstronaut(int index)
+        {
+            Astronauts.RemoveAt(index);
+        }
+        public bool LandSafely()
+        {
+            foreach(Astronaut aAstronaut in Astronauts)
+            {
+                if (aAstronaut.IsTraitor)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
